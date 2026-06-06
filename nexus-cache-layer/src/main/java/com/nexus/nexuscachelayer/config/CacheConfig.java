dc1b2209b96a0ada57d377c89682dbda4dd8dff9
elton <eltonguambe1006@gmail.com>
2026-06-06 09:12:36 +0200

    feat: cache module -> CacheConfig

---
+++ nexus-cache-layer/src/main/java/com/nexus/nexuscachelayer/config/CacheConfig.java
@@ -0,0 +1,23 @@
+package com.nexus.nexuscachelayer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        return template;
+    }
+
+}
